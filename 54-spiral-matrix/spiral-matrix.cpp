443b630bea2c252fa780f41dc6ea8f8a55db85ef
Nachiket Kale <nachiket.kale24@spit.ac.in>
2026-09-25 18:12:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (55.96%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,52 +1,34 @@
 class Solution {
 public:
-    // Function ka naam 'ans' se badal kar 'solve' kar diya
-    void solve(vector<vector<int>>& matrix, int r, int c, int dir, vector<int>& res, vector<vector<int>>& visited) {
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        
-        // Current cell ko result mein daalo aur visited mark karo
-        res.push_back(matrix[r][c]);
-        visited[r][c] = 1;
-        
-        // 4 Directions: Right(0), Down(1), Left(2), Up(3)
-        // dr aur dc hume batate hain ki x aur y axis pe kahan jana hai
-        int dr[] = {0, 1, 0, -1};
-        int dc[] = {1, 0, -1, 0};
-        
-        // Pehle current direction mein aage badhne ki koshish karo
-        int nr = r + dr[dir];
-        int nc = c + dc[dir];
-        
-        // Agar aage ka rasta safe hai aur visited nahi hai, toh aage badho
-        if (nr >= 0 && nr < m && nc >= 0 && nc < n && visited[nr][nc] == 0) {
-            solve(matrix, nr, nc, dir, res, visited);
-        } else {
-            // Agar deewar aagayi ya visited array mil gaya, toh 90 degree turn lo
-            dir = (dir + 1) % 4;
-            nr = r + dr[dir];
-            nc = c + dc[dir];
-            
-            // Turn lene ke baad check karo ki valid rasta bacha hai ya end aagaya
-            if (nr >= 0 && nr < m && nc >= 0 && nc < n && visited[nr][nc] == 0) {
-                solve(matrix, nr, nc, dir, res, visited);
+
+        vector<int> ans;
+        vector<vector<int>> visited_matrix(m,vector<int>(n,0));
+        if (matrix.empty()) return ans;
+        vector<int> dr = {0,1,0,-1};
+        vector<int> dc = {1,0,-1,0};
+
+        int a = 0;  //direction
+        int b = 0;   //initial valuess of starting point
+        int c = 0;
+
+        for(int i=0;i<m*n;i++){
+            ans.push_back(matrix[b][c]);
+            visited_matrix[b][c] = 1;
+            int next_dr = b + dr[a];
+            int next_dc = c + dc[a];
+            if(next_dr>=0 && next_dr<m && next_dc>=0 && next_dc<n && visited_matrix[next_dr][next_dc]!=1){
+                b = next_dr;
+                c = next_dc;
+            }
+            else{
+                a = (a+1)%4;   //rotating 90 degree
+                b = b + dr[a];
+                c = c + dc[a];
             }
         }
-    }
-
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> res;
-        if (matrix.empty()) return res;
-        
-        int m = matrix.size();    // rows
-        int n = matrix[0].size(); // columns
-        
-        // visited array ko size ke BAAD define kiya
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-        
-        // Start from row 0, col 0, facing Right (direction 0)
-        solve(matrix, 0, 0, 0, res, visited);
-        
-        return res;
+        return ans;
     }
 };
