9359f64b35db3b6f2d37fad3f6415b643af6e908
Nachiket Kale <nachiket.kale24@spit.ac.in>
2026-09-26 10:56:41 +0530

    Time: 932 ms (5.01%) | Memory: 82.8 MB (5.05%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> ans;
         int n = nums.size();
-        
-        sort(nums.begin(), nums.end()); // O(N log N)
+        set<vector<int>> st; // Unique quadruplets filter karne ke liye
         
         for(int i = 0; i < n; i++) {
-            // Avoid duplicates for 'i'
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
             for(int j = i + 1; j < n; j++) {
-                // Avoid duplicates for 'j'
-                if(j > i + 1 && nums[j] == nums[j-1]) continue;
-                
-                int left = j + 1;
-                int right = n - 1;
-                
-                while(left < right) {
-                    // Use long long to avoid LeetCode's integer overflow trap
-                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
-                    
-                    if(sum == target) {
-                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
-                        left++;
-                        right--;
+                for(int k = j + 1; k < n; k++) {
+                    for(int l = k + 1; l < n; l++) {
                         
-                        // Avoid duplicates for 'left' and 'right'
-                        while(left < right && nums[left] == nums[left-1]) left++;
-                        while(left < right && nums[right] == nums[right+1]) right--;
-                    } 
-                    else if(sum < target) {
-                        left++;
-                    } 
-                    else {
-                        right--;
+                        long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+                        
+                        if(sum == target) {
+                            vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                            sort(temp.begin(), temp.end()); // Set mein duplicate arrays rokne ke liye sorting zaroori hai
+                            st.insert(temp);
+                        }
                     }
                 }
             }
         }
+        
+        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
